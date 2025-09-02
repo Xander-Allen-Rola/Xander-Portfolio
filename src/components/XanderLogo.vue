01f372e50845c7a9ae08f57cc3c797f477dffcb7
Xander Allen Rola <20190016434@my.xu.edu.ph>
2025-09-03 00:01:52 +0800

    1. Deleted unnecessary default files from Vue 2. Added Xander logo

---
+++ src/components/XanderLogo.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="xander-logo">
+        <img alt="X logo" src="@/assets/icons/X_icon.png" />
+        <span class="ander">ander</span>
+    </div>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap');
+.ander {
+    font-family: 'Poppins', sans-serif;
+}
+.xander-logo {
+    display: flex;
+    align-items: center;
+}
+</style>
