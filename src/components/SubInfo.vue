fc837c6b0d672829734e95d94511a40c85511f45
Xander Allen Rola <20190016434@my.xu.edu.ph>
2025-09-04 22:22:53 +0800

    added star image between each sub info text

--- src/components/SubInfo.vue
+++ src/components/SubInfo.vue
@@ -2,8 +2,11 @@
   <hr class="horizontal-line" />
     <div class="subinfo-text">
         <p>JavaScript</p>
+        <img src="@/assets/icons/star.png" />
         <p>Tailwind CSS</p>
+        <img src="@/assets/icons/star.png" />
         <p>Figma</p>
+        <img src="@/assets/icons/star.png" />
         <p>GitHub</p>
         </div>
   <hr class="horizontal-line" />
@@ -11,14 +14,20 @@
 
 <style scoped>
 .horizontal-line {
-  border: 2px solid #ccc;
+  border: 1px solid rgba(255, 255, 255, 0.2);
   width: 100vw; /* Full viewport width */
 }
 .subinfo-text {
     display: flex;
-    gap: 20rem; /* Adjust the value as needed */
+    gap: 9rem; /* Adjust the value as needed */
     font-size: 25px; /* Adjust as needed */
-    margin-top: 0rem;    /* Space above */
-    margin-bottom: 0rem; /* Space below */
+    margin-top: 1rem;    /* Space above */
+    margin-bottom: 1rem; /* Space below */
+    white-space: nowrap; /* Prevent text wrapping */
+}
+.subinfo-text img {
+    width: 28px;   /* Set your desired width */
+    height: 38px;  /* Set your desired height */
+    object-fit: contain;
 }
 </style>
