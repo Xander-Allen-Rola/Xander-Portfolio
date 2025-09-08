16af7d1e7d55bc7b024ebe404896f93635669d07
Xander Allen Rola <20190016434@my.xu.edu.ph>
2025-09-08 20:39:43 +0800

    Refactored Title text: moved style inside template

--- src/components/TitleText.vue
+++ src/components/TitleText.vue
@@ -3,37 +3,24 @@ import ProfileButton from './buttons/HireMeButton.vue';
 </script>
 
 <template>
-  <div class="title-text">
-    <h1>Xander Allen Rola</h1>
-    <p>Front-End Developer | Passion for Clean Code and Creative Solutions</p>
-    <ProfileButton label="Hire Me" class="oblong-btn" />
-  </div>
-</template>
-
-<style scoped>
-.title-text {
-  display: flex;
+  <div 
+  style="display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   height: 100%;
-  text-align: center;
-}
-
-.title-text h1 {
-  font-size: 2.5rem;
-  margin-bottom: 1rem;
-  color: #fff;
-}
-
-.title-text p {
-  font-size: 1.25rem;
-  color: #ddd;
-}
-
-.title-text .oblong-btn {
-  margin-top: 2rem;
-  width: 160px;
-  font-size: 1.1rem;
-}
-</style>
+  text-align: center;">
+    <h1 
+    style="font-size: 2.5rem;
+    margin-bottom: 1rem;
+    color: #fff;">Xander Allen Rola</h1>
+    <p 
+    style="font-size: 1.25rem;
+    color: #ddd;">Front-End Developer | Passion for Clean Code and Creative Solutions</p>
+    <ProfileButton 
+    label="Hire Me" 
+    style="margin-top: 2rem;
+    width: 160px;
+    font-size: 1.1rem;" />
+  </div>
+</template>
