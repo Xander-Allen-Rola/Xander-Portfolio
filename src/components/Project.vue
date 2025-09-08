cc3b879ec861f1f38aec7bec67517df0ddd3e118
Xander Allen Rola <20190016434@my.xu.edu.ph>
2025-09-08 20:16:48 +0800

    Refactored Project code: moved styling to template

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -8,39 +8,27 @@ defineProps({
 </script>
 
 <template>
-    <div class="project-box">
-        <div class="content-container">
+    <div 
+    style="background: #151515;
+    border-radius: 8px;
+    padding: 20px;
+    height: 500px;
+    width: 95%;">
+        <div 
+        style="display: flex;
+        align-items: center;
+        height: 100%;
+        gap: 90px;">
             <img :src="image" style="height:95%; margin-left: 30px;"/>
-            <div class="text-container">
-                <h1>{{ name }}</h1>
+            <div 
+            style="display: flex;
+            flex-direction: column;
+            gap: 20px;
+            max-width: 600px;">
+                <h1 style="font-weight: bold;">{{ name }}</h1>
                 <p>{{ description }}</p>
                 <ViewProject />
             </div>
         </div>
     </div>
 </template>
-
-<style scoped>
-.project-box {
-  background: #151515;
-  border-radius: 8px;
-  padding: 20px;
-  height: 500px;
-  width: 95%;
-}
-.content-container {
-  display: flex;
-  align-items: center;
-  height: 100%;
-  gap: 90px;
-}
-.text-container {
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  max-width: 600px;
-}
-h1{
-    font-weight: bold;
-}
-</style>
