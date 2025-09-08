91bbbeb7a16a86809855ce04649d759cfc09fdb9
Xander Allen Rola <20190016434@my.xu.edu.ph>
2025-09-08 20:34:22 +0800

    Refactored SubInfo code: moved style code inside template

--- src/components/SubInfo.vue
+++ src/components/SubInfo.vue
@@ -1,33 +1,32 @@
 <template>
-  <hr class="horizontal-line" />
-    <div class="subinfo-text">
-        <p>JavaScript</p>
-        <img src="@/assets/icons/star.png" />
-        <p>Tailwind CSS</p>
-        <img src="@/assets/icons/star.png" />
-        <p>Figma</p>
-        <img src="@/assets/icons/star.png" />
-        <p>GitHub</p>
-        </div>
-  <hr class="horizontal-line" />
-</template>
-
-<style scoped>
-.horizontal-line {
-  border: 1px solid rgba(255, 255, 255, 0.2);
-  width: 100vw; /* Full viewport width */
-}
-.subinfo-text {
-    display: flex;
+  <hr 
+  style="border: 1px solid rgba(255, 255, 255, 0.2);
+  width: 100vw; /* Full viewport width */" />
+    <div 
+    style="display: flex;
     gap: 9rem; /* Adjust the value as needed */
     font-size: 25px; /* Adjust as needed */
     margin-top: 1rem;    /* Space above */
     margin-bottom: 1rem; /* Space below */
-    white-space: nowrap; /* Prevent text wrapping */
-}
-.subinfo-text img {
-    width: 28px;   /* Set your desired width */
-    height: 38px;  /* Set your desired height */
-    object-fit: contain;
-}
-</style>
+    white-space: nowrap; /* Prevent text wrapping */">
+        <h3>JavaScript</h3>
+        <img src="@/assets/icons/star.png"
+        style="width: 28px;   /* Set your desired width */
+        height: 38px;  /* Set your desired height */
+        object-fit: contain;" />
+        <h3>Tailwind CSS</h3>
+        <img src="@/assets/icons/star.png"
+        style="width: 28px;   /* Set your desired width */
+        height: 38px;  /* Set your desired height */
+        object-fit: contain;" />
+        <h3>Figma</h3>
+        <img src="@/assets/icons/star.png"
+        style="width: 28px;   /* Set your desired width */
+        height: 38px;  /* Set your desired height */
+        object-fit: contain; " />
+        <h3>GitHub</h3>
+        </div>
+  <hr 
+  style="border: 1px solid rgba(255, 255, 255, 0.2);
+  width: 100vw; /* Full viewport width */" />
+</template>
